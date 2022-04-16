558de82f478f2b690f2fc4150c22099154c880d0
Rishabh1Tyagi <rishav.tyagi19@gmail.com>
2022-04-16 20:30:01 +0530

    login Page

---
+++ src/components/ShoppingCart/ShoppingCart.js
@@ -0,0 +1,14 @@
+// s
+import React from "react";
+
+const ShoppingCart = () => {
+    return(
+        <>
+         <div>
+             <h1>ShoppingCart page here</h1>
+         </div>
+        </>
+    )
+}
+
+export default ShoppingCart;
