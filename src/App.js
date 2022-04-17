08a4f29548b41e64d089a031919f8f44acce0deb
Rishabh1Tyagi <rishav.tyagi19@gmail.com>
2022-04-17 17:15:40 +0530

    added product page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import Login from "./components/Login/Login";
 import ShoppingCart from "./components/ShoppingCart/ShoppingCart";
+import NoPage from "./components/NotFound/NoPage";
 import { Route, Routes } from 'react-router-dom';
 
 
@@ -12,8 +13,10 @@ function App() {
     <Header />
     <main>
       <Routes>
+        <Route path="/" element={<Login/>} />
         <Route path="/login" element={<Login/>} />
         <Route path="/shoppingCart" element={<ShoppingCart/>} />
+        <Route path="*" element={<NoPage/>} />
       </Routes>
     </main>
     </div>
